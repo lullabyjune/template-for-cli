0c7a5c971b3445573c72e6c8dd9833740e33e131
yangjian6 <yangjian6@xiaomi.com>
2020-01-08 10:15:43 +0800

    add typescript babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react'
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage'
+      }
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript'
   ],
   plugins: []
 }
